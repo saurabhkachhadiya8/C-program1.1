e7a7eb81333b6433a38a8996a8f35cbc62e4d099
theAkHilsarkar18 <akhilsodvadiya7@gmail.com>
2024-01-16 17:11:20 +0530

    C-program1.1

---
+++ program1.1(Swastik).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+main()
+{
+	printf("\n\n\t     Swastik\n\n\n");
+	
+	printf("\t*\t* * * * *\n");
+	printf("\t*\t*\n");
+	printf("\t*\t*\n");
+	printf("\t*\t*\n");
+	printf("\t* * * * * * * * *\n");
+	printf("\t\t*\t*\n");
+	printf("\t\t*\t*\n");
+	printf("\t\t*\t*\n");
+	printf("\t* * * * *\t*\n\n\n\n");
+
+}
---
+++ program1.2(Triangle).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+	printf("\n\n\t      triangle pattern\n\n\n");
+	
+	printf("\t\t     *\n");
+	printf("\t\t    ***\n");
+	printf("\t\t   *****\n");
+	printf("\t\t  *******\n");
+	printf("\t\t *********\n");
+	printf("\t\t***********\n\n\n\n");
+
+}
